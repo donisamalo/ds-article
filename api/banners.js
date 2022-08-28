f2503536aa9699a0ef0604c27ef028ffcc457c7f
Doni Samalo <doni.samalo@mekari.com>
2022-08-29 03:00:35 +0700

    fix: add default env api base url

--- api/banners.js
+++ api/banners.js
@@ -1,4 +1,5 @@
-const baseUrl = process.env.API_BASE_URL
+const baseUrl =
+  process.env.API_BASE_URL || 'https://62d5368fd4406e5235558a46.mockapi.io'
 const url = `${baseUrl}/banners`
 
 export default (axios) => ({
